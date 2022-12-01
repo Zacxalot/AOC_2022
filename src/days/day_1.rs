16ea74ecfc7cde23f03fa2f278d1b2163b06cbf2
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2022-12-01 08:50:55 +0000

    Day 1 part 2

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -11,24 +11,30 @@ pub fn execute() -> Answer {
         .map(|line| line.to_owned())
         .collect::<Vec<String>>();
 
+    let mut highest_totals: [i32; 3] = [0; 3];
+
     let mut chunk_total = 0;
-    let mut highest_total = 0;
     for line in lines {
         if line.trim().is_empty() {
-            if chunk_total > highest_total {
-                highest_total = chunk_total
+            if chunk_total >= highest_totals[2] {
+                highest_totals[0] = highest_totals[1];
+                highest_totals[1] = highest_totals[2];
+                highest_totals[2] = chunk_total;
+            } else if chunk_total >= highest_totals[1] {
+                highest_totals[0] = highest_totals[1];
+                highest_totals[1] = chunk_total;
+            } else if chunk_total > highest_totals[0] {
+                highest_totals[0] = chunk_total;
             }
-
-            chunk_total = 0
+            chunk_total = 0;
         } else {
-            println!("{}", line);
             chunk_total += line.trim().parse::<i32>().unwrap();
         }
     }
 
-    let part_1 = highest_total.to_string();
+    let part_1 = highest_totals[2].to_string();
 
-    let part_2 = "Part 2".to_owned();
+    let part_2 = highest_totals.iter().sum::<i32>().to_string();
 
     let duration = Instant::now() - time_before;
 
