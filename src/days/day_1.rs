e19ed3dd6f651d617c85182c0866f8a8fe7bbd24
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2022-12-01 08:38:55 +0000

    Day 1 part 1

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,11 +1,32 @@
-use std::time::Instant;
+use std::{fs, time::Instant};
 
 use crate::Answer;
 
 pub fn execute() -> Answer {
     let time_before = Instant::now();
 
-    let part_1 = "Part 1".to_owned();
+    let lines = fs::read_to_string("./input/day_1.txt")
+        .unwrap()
+        .split('\n')
+        .map(|line| line.to_owned())
+        .collect::<Vec<String>>();
+
+    let mut chunk_total = 0;
+    let mut highest_total = 0;
+    for line in lines {
+        if line.trim().is_empty() {
+            if chunk_total > highest_total {
+                highest_total = chunk_total
+            }
+
+            chunk_total = 0
+        } else {
+            println!("{}", line);
+            chunk_total += line.trim().parse::<i32>().unwrap();
+        }
+    }
+
+    let part_1 = highest_total.to_string();
 
     let part_2 = "Part 2".to_owned();
 
