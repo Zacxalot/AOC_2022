0adb320263ca6d66944736b50259a4dd811d5cd7
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2022-12-05 19:08:04 +0000

    Day 5 part 2

--- src/days/day_5.rs
+++ src/days/day_5.rs
@@ -17,6 +17,7 @@ pub fn execute() -> Answer {
         / 3;
 
     let mut crates: Vec<Vec<char>> = vec![vec![]; stack_count];
+    let mut part_2_crates: Vec<Vec<char>> = vec![vec![]; stack_count];
 
     let crates_lines = file.lines().filter(|line| line.contains('[')).rev();
 
@@ -24,7 +25,8 @@ pub fn execute() -> Answer {
         for (index, mut chunk) in line.chars().chunks(4).into_iter().enumerate() {
             let id = chunk.nth(1).unwrap();
             if !id.is_whitespace() {
-                crates[index].push(id)
+                crates[index].push(id);
+                part_2_crates[index].push(id);
             }
         }
     }
@@ -42,7 +44,7 @@ pub fn execute() -> Answer {
         })
         .collect::<Vec<(usize, usize, usize)>>();
 
-    for (count, from, to) in instructions {
+    for (count, from, to) in &instructions {
         let from_len = crates[from - 1].len();
         let mut to_move = crates[from - 1]
             .drain((from_len - count)..)
@@ -59,6 +61,21 @@ pub fn execute() -> Answer {
         .filter(|c| !c.is_whitespace())
         .collect::<String>();
 
+    for (count, from, to) in instructions {
+        let from_len = part_2_crates[from - 1].len();
+        let mut to_move = part_2_crates[from - 1]
+            .drain((from_len - count)..)
+            .collect::<Vec<char>>();
+
+        part_2_crates[to - 1].append(&mut to_move);
+    }
+
+    let part_2 = part_2_crates
+        .iter()
+        .map(|stack| stack.last().unwrap_or(&' '))
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>();
+
     // for instruction in instructions {
     //     println!("{:?}", instruction);
     // }
@@ -69,7 +86,7 @@ pub fn execute() -> Answer {
     Answer {
         day: 5,
         part_1,
-        part_2: "2".to_string(),
+        part_2,
         duration,
         no_io_duration,
     }
